14545d6c5285ae3132ed66e375b3121ff87f0413
IqbalAlFikri <iqbalalfikri01072004@gmail.com>
2022-12-16 19:32:11 +0700

    komentar

--- DDP/Pertemuan ke 12/menjalankanmodul.py
+++ DDP/Pertemuan ke 12/menjalankanmodul.py
@@ -1,4 +1,4 @@
-# memanggil dan menjalankan modul P12modulBangunDatar
+# memanggil dan menjalankan modul modulbangundatar
 print("====================Modul Bangun Datar====================")
 import modulbangundatar as hasil
 hasil.lingkaran(10)
@@ -6,14 +6,3 @@ hasil.persegi(7)
 hasil.persegiPanjang(3, 6)
 hasil.segitiga(4, 6)
 hasil.belahKetupat(4, 2)
-
-print(" ")
-print(" ")
-print(" ")
-print(" ")
-
-from modularitmatika import tambah, kurang, kali
-print("=====================Modul Aritmatika=====================")
-tambah(5, 3, 7, 8, 10)
-kurang(12, 24, 35)
-kali(5, 4, 9, 7)
---
+++ DDP/Pertemuan ke 12/menjalankanmodul2.py
@@ -0,0 +1,5 @@
+from modularitmatika import tambah, kurang, kali
+print("=====================Modul Aritmatika=====================")
+tambah(5, 3, 7, 8, 10)
+kurang(12, 24, 35)
+kali(5, 4, 9, 7)
